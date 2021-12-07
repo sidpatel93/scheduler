88564f2e0885a4cfbbce9cc235b057fd97c42645
Siddharth Patel <sid.auburn@gmail.com>
2021-12-07 14:36:33 -0500

    add state in the application to track the selected day

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,9 +4,11 @@ import 'components/DayListItem.scss'
 
 export default function DayListItem(props) {
 
+  const {selected, spots, name, setDay} = props
+
   let dayClass = classNames("day-list__item", {
-   "day-list__item--selected": props.selected,
-   "day-list__item--full": props.spots===0
+   "day-list__item--selected": selected,
+   "day-list__item--full": spots===0
   })
 
   const formatSpots = (spots) => {
@@ -19,9 +21,9 @@ export default function DayListItem(props) {
     return `${spots} spots remaining`
   }
   return (
-    <li className={dayClass} onClick={()=>{props.setDay(props.name)}} >
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    <li className={dayClass} onClick={()=>{setDay(name)}} >
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
