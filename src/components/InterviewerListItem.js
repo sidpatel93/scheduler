4fe6a13b3cec3420a6ad302993565c2bfd487a7d
Siddharth Patel <sid.auburn@gmail.com>
2021-12-08 08:14:32 -0500

    finish interviewer component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+import './InterviewerListItem.scss'
+
+const InterviewerListItem = (props) => {
+  const {id, name, avatar, selected, setInterviewer} = props
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected
+  })
+  return(
+    <li className={interviewerClass} onClick={()=>{setInterviewer(id)}}>
+  <img
+    className="interviewers__item-image"
+    src={avatar}
+    alt={name}
+  />
+  {selected && name}
+</li>
+  )
+}
+
+export default InterviewerListItem;
