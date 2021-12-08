6fb0baa4987ac6def8ced716da77d855e403092a
Siddharth Patel <sid.auburn@gmail.com>
2021-12-08 15:52:37 -0500

    populate the appointments in the application with fake static data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "./DayList"
+import Appointment from "./Appointment/index";
+
 const days = [
   {
     id: 1,
@@ -18,6 +20,51 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+const appointmentArray = appointments.map(appointment => {
+  return(
+    <Appointment key={appointment.id} {...appointment} />
+  )
+})
 
 export default function Application(props) {
 
@@ -47,7 +94,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentArray}
       </section>
     </main>
   );
