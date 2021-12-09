a6b91ed11c6ebb91ce609b0e6d853a8b826e02f5
Siddharth Patel <sid.auburn@gmail.com>
2021-12-09 09:47:11 -0500

    removed the hardcoded days and instead use axios lib

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 import "components/Application.scss";
 import DayList from "./DayList"
 import Appointment from "./Appointment/index";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -69,6 +52,15 @@ const appointmentArray = appointments.map(appointment => {
 export default function Application(props) {
 
   const [daySelected, setdaySelected] = useState("Monday")
+
+  const [days, setDays] = useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:8001/api/days")
+    .then(res => {
+      setDays(res.data)
+    })
+  },[])
 
   return (
     <main className="layout">
