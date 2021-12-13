e5de4cc8e006f3303129eb52ea672e1143608baf
Siddharth Patel <sid.auburn@gmail.com>
2021-12-13 10:01:02 -0500

    add status component while saving

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React, {Fragment} from "react"
 import './styles.scss'
 import Header from "./Header"
 import Show from "./Show"
+import Status from "./Status"
 import Empty from "./Empty"
 import useVisualMode from 'hooks/useVisualMode'
 import Form from "./Form"
@@ -11,6 +12,7 @@ const Appointment = (props) => {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const {id, time, interview, interviewers, bookInterview} = props
   const {mode, transition, back} = useVisualMode(
@@ -21,6 +23,7 @@ const Appointment = (props) => {
       student: name,
       interviewer
     };
+    transition(SAVING)
     bookInterview(props.id, interview)
     .then(()=>transition(SHOW))
 
@@ -31,6 +34,7 @@ const Appointment = (props) => {
     <article className="appointment">
       <Header time={time} />
       {mode===EMPTY && <Empty onAdd={()=> transition(CREATE)}/>}
+      {mode === SAVING && <Status message="SAVING..." />}
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/> }
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={()=>back()} onSave={save} />}
     </article>
