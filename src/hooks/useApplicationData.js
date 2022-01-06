1afd783d2a946cd87a7fe3823e25bacd75cd6f39
Siddharth Patel <sid.auburn@gmail.com>
2022-01-06 17:26:24 -0500

    update the api url to fetch data from heroku server

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -53,7 +53,7 @@ const useApplicationData = () => {
       ...state.appointments,
       [id]: appointment
     };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    return axios.delete(`/api/appointments/${id}`)
     .then(res => console.log(res))
     .then(() => {
       const days = updateSpots(state,appointments,id)
@@ -72,7 +72,7 @@ const useApplicationData = () => {
       ...state.appointments,
       [id]: appointment
     };
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: interview})
+    return axios.put(`/api/appointments/${id}`, {interview: interview})
     .then(res => console.log(res))
     .then(() => {
       const days = updateSpots(state,appointments,id)
@@ -85,9 +85,9 @@ const useApplicationData = () => {
 
   useEffect(()=>{
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       //console.log(all)
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
